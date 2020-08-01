45e52c8e95f68e8e5a9620cf33b47c95c145d62e
Wareares <43419882+wareares@users.noreply.github.com>
2020-08-02 02:58:31 +0700

    v1

---
+++ config.py
@@ -0,0 +1,32 @@
+
+account = {
+	"Username":"utsjbbs",
+	"Password":"utjsbbb"
+}
+
+
+bet = {
+	"Basebet":"0.001",
+	"Chance1":"53",
+	"Chance2":"53"
+}
+
+
+
+betset = {
+	"IFLose":"2.65",
+	"IFWin":"2",
+	"TargetProfit":"0.001",
+	"TargetLose":"1000000",
+	"ResetBetIfWin":"1",
+	"ResetBetIfLose":"0",
+	"ResetSeedEachBet":"ON"
+}
+
+
+withdraw = {
+	"AutoWithdraw":"OFF",
+	"AmountWithdraw":"0",
+	"WithdrawIfBalance":"0",
+	"Address":""
+}
